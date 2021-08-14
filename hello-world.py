561e0736b937c3f43f88971bad5d6f7692afe889
SasiKanth <itz.sashi@gmail.com>
2021-08-14 07:26:31 +0530

    hello world program updated - data types

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,8 @@
 print("Hello World")
 print("Test Message")
-print(2+3)
-print(3/2)
-print(3//2)
-print(3%2)
-print(3*2)
+print(2+3) #addition
+print(3/2) #gives output with decimals
+print(3//2) #roundoff the output
+print(3%2) #modular (reminder)
+print(3*2) #multiply
+print("square is: 3**2") #square
---
+++ vars-datatypes.py
@@ -0,0 +1,18 @@
+first_number = 6
+second_number = 8
+print(first_number+second_number)
+type(first_number)
+myFloat = 3.1514
+type(myFloat)
+
+# ###
+# one=10
+# two=20
+# type(one)          ; This is to print the type of variable/value, in this case its integer
+# <class 'int'>
+# myFloat = 3.2423
+# type(myFloat)      ; float value
+# <class 'float'>
+# myDecision = False
+# type(myDecision)    ; boolian
+# <class 'bool'>
