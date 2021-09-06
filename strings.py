e5f8cf1711d80a007030c246b4d2d53a873817ac
SasiKanth <itz.sashi@gmail.com>
2021-09-06 12:15:53 +0530

    String concatination added

---
+++ strings.py
@@ -0,0 +1,6 @@
+#String concatinations
+f_name = 'Sasi'
+l_name = "Kanth"
+print(f_name+l_name)
+print(f_name+" "+l_name)
+print('4'+'3')  #Here 4 & 3 are not considered as numbers/integers. They given in '' - so considered as strings.
